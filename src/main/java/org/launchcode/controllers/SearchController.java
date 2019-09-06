c6ed893fdc73d29a799d84c26863d7cd40b80e2d
Aaron Link <aaron@thelinkfamily.net>
2019-09-05 19:44:33 -0500

    Final Commit of TechJobs-MVC

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -23,5 +24,23 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String searchResults(HttpServletRequest request, Model model) {
+        String searchType = request.getParameter("searchType");
+        String searchTerm = request.getParameter("searchTerm");
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 
 }
